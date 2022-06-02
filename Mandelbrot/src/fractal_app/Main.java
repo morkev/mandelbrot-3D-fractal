c847c988bd7c920149a4370e78a1b1b88d796ae7
Kevin Mora <83437383+morkev@users.noreply.github.com>
2022-06-01 21:49:45 -0600

    Final Version :: Java Mandelbrot Visualization

--- Mandelbrot/src/fractal_app/Main.java
+++ Mandelbrot/src/fractal_app/Main.java
@@ -49,7 +49,6 @@ public class Main extends JFrame {
         this.setSize(WIDTH, HEIGHT);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-
     }
 
     // -----------------------------------------------------------------------------
@@ -97,16 +96,16 @@ public class Main extends JFrame {
     }
 
     private int computeIterations(double c_r, double c_i) {
-		/*
-		Let c = c_r + c_i
-		Let z = z_r + z_i
-
-		z' = z*z + c
-		   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
-		   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
-		     z_r' = z_r² - z_i² + c_r
-		     z_i' = 2*z_i*z_r + c_i
-		*/
+	/*
+	Let c = c_r + c_i
+	Let z = z_r + z_i
+	
+	z' = z*z + c
+	   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
+ 	   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
+	     z_r' = z_r² - z_i² + c_r
+	     z_i' = 2*z_i*z_r + c_i
+	*/
 
         double z_r    = 0.0;
         double z_i    = 0.0;
@@ -243,5 +242,3 @@ public class Main extends JFrame {
     }
 
 }
-
-
